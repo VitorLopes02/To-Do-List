5642244591243fda4ae39f326393131dc083924d
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2023-10-13 17:25:40 -0300

    Project-Structure

---
+++ src/test/java/br/com/papiro/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.papiro.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
