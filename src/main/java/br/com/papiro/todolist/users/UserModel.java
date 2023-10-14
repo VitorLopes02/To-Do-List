1ccda2abd0b56897d4453d435277adefd97aedbc
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2023-10-14 11:36:37 -0300

    Adicionando novas dependencias no pom

---
+++ src/main/java/br/com/papiro/todolist/users/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.papiro.todolist.users;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String password;
+
+    
+}
---
+++ src/main/java/br/com/papiro/todolist/users/UserRepository.java
@@ -0,0 +1,5 @@
+package br.com.papiro.todolist.users;
+
+public class UserRepository {
+    
+}
